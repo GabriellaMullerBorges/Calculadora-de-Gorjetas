072bccbe548b50deb92c544a31bed0299ec43f1e
Gabriella Borges <gabimullerborges@gmail.com>
2023-05-25 13:39:54 -0300

    alterações no arquivo JS para implementação da funcionalidade do cálculo por meiod e alert

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// INPUT DO VALOR DA CONTA E Nº PESSOAS //
+
 const billInput = document.querySelector('#input-bill');
 const peopleInput = document.querySelector('#input-people');
 
@@ -12,6 +14,44 @@ peopleInput.addEventListener('input',function(event){
     peopleEntry = event.target.value;
 });
 
-var bill = billEntry;
-var people = peopleEntry;
 
+// BOTÕES DE PORCENTAGEM //
+
+var porcentagemSelecionada;
+var valorNumericoSelecionado;
+
+const botoes = document.querySelectorAll('.botao-tip');
+
+    botoes.forEach(botao => {
+        
+        botao.addEventListener('mouseup', function() {
+            if (billEntry == "" || peopleEntry === "") { 
+                alert('Ops, algum campo não foi preenchido devidamente')  
+           } else {  
+            porcentagemSelecionada = botao.dataset.porcentagem;
+             valorNumericoSelecionado = parseFloat(porcentagemSelecionada.replace('%', '')); 
+
+             var  tip = (billEntry*valorNumericoSelecionado)/100;
+             var tipPerson = (tip/peopleEntry);
+
+             var  billValue = parseFloat(billEntry);
+             var  peopleValue = parseInt(peopleEntry);
+
+             var totalPorPessoa = (billValue+tip)/peopleValue;
+
+             console.log(
+            `VALOR porcentagem ${valorNumericoSelecionado} TIPO ${typeof valorNumericoSelecionado}!
+             VALOR BillEntry é ${billEntry} TIPO ${typeof billEntry}! 
+             VALOR peopleEntry ${peopleEntry}TIPO ${ typeof peopleEntry}!
+             VALOR tip ${tip} TIPO ${typeof tip}! 
+             VALOR tip/person ${tipPerson} TIPO ${typeof tipPerson}!
+             VALOR billValue ${billValue} TIPO ${typeof billValue}!
+             VALOR peopleValue ${peopleValue} TIPO ${typeof peopleValue}!
+             VALOR totalPorPessoa ${totalPorPessoa} TIPO ${typeof totalPorPessoa}!
+             `
+             );        
+             
+             alert(`O valor de gorjeta por pessoa é ${tipPerson} e o total por pessoa ${totalPorPessoa}`)
+           }   
+        }  
+    )});
