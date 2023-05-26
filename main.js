96b61bdc1a35cf0e4e69bc70fb003d2d3f894dce
Gabriella Borges <gabimullerborges@gmail.com>
2023-05-26 16:37:07 -0300

    feat( Corrigida a função do input Custom e do botão Reset quanto a ele)

--- main.js
+++ main.js
@@ -21,26 +21,23 @@ peopleInput.addEventListener('input',function(event){
 });
 
   
-// BOTÕES DE PORCENTAGEM E FUNÇÃO //
+// FUNÇÃO QUE CALCULA A GORJETA//
 
 var porcentagemSelecionada;
 var valorNumericoSelecionado;
 
-const botoes = document.querySelectorAll('.botao-tip');
-
-    botoes.forEach(botao => {
-        
-        botao.addEventListener('mouseup', function() {
-            if (billEntry == "" || peopleEntry === "" || billEntry == "0" || peopleEntry === "0") { 
+function calculaGorjeta(gorjeta){
+     if (billEntry == "" || peopleEntry === "" || billEntry == "0" || peopleEntry === "0") { 
                 alert('Ops, algum campo não foi preenchido devidamente')  
            } else if(/[^0-9.,]/.test(billEntry) || /\D/.test(peopleEntry)){ 
             alert('Ops, preencha adequadamente os campos com números')  
            }else if(!/^\d+([.,]\d{1,2})?$/.test(billEntry)){
             alert('Ops, preencha apenas até duas casas após a vírgula')  
            }else if(billEntry.includes(",")){
-             var billEntrada = billEntry.replace(',','.')
 
-             porcentagemSelecionada = botao.dataset.porcentagem;
+            var billEntrada = billEntry.replace(',','.')
+
+             porcentagemSelecionada = gorjeta.dataset.porcentagem;
              valorNumericoSelecionado = parseFloat(porcentagemSelecionada.replace('%', '')); 
 
              var  tip = (billEntrada*valorNumericoSelecionado)/100;
@@ -54,9 +51,10 @@ const botoes = document.querySelectorAll('.botao-tip');
              tipAmountOutput.innerHTML = `$ ${tipPerson.toFixed(2)}`;
              totalOutput.innerHTML =  `$ ${totalPorPessoa.toFixed(2)}`;
            }else {  
-             porcentagemSelecionada = botao.dataset.porcentagem;
-             valorNumericoSelecionado = parseFloat(porcentagemSelecionada.replace('%', '')); 
+             porcentagemSelecionada = gorjeta.dataset.porcentagem;
+             valorNumericoSelecionado = parseFloat(porcentagemSelecionada.replace('%', ''));
 
+             
              var  tip = (billEntry*valorNumericoSelecionado)/100;
              var tipPerson = (tip/peopleEntry);
 
@@ -67,10 +65,54 @@ const botoes = document.querySelectorAll('.botao-tip');
 
              tipAmountOutput.innerHTML = `$ ${tipPerson.toFixed(2)}`;
              totalOutput.innerHTML =  `$ ${totalPorPessoa.toFixed(2)}`;
+}
+}
+
+const botoes = document.querySelectorAll('.botao-tip:not([class*=" "])');
+
+    botoes.forEach(botao => {
+        
+        botao.addEventListener('mouseup', function() {
+            calculaGorjeta(botao);
            }   
-        }  
-    )});  
+     )});
     
+    //BOTAO CUSTOM//
+
+const inputCustom = document.querySelector('.input-custom');
+
+function verificaNAN(valor){
+    if(isNaN(valor) || valor == 0){
+        inputCustom.parentElement.dataset.porcentagem = '';
+        alert("Ops!")  
+    }else{
+        inputCustom.parentElement.dataset.porcentagem = valor;
+
+        var billEntrada = billEntry.replace(',','.');
+        var  tip = (billEntrada*valor)/100;
+        var tipPerson = (tip/peopleEntry);
+
+        var  billValue = parseFloat(billEntrada);
+        var  peopleValue = parseInt(peopleEntry);
+
+        var totalPorPessoa = (billValue+tip)/peopleValue;
+
+        tipAmountOutput.innerHTML = `$ ${tipPerson.toFixed(2)}`;
+        totalOutput.innerHTML =  `$ ${totalPorPessoa.toFixed(2)}`;
+    }
+}
+
+inputCustom.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+
+    const userInput = inputCustom.value;
+    const valorNumerico = parseFloat(userInput.replace('%', '')/*.replace(/,/g, '.')*/); 
+    
+    verificaNAN(valorNumerico)     
+    }
+ }
+) 
+
     //RESETAR//
 
     function resetPlaceholders(){
@@ -78,9 +120,11 @@ const botoes = document.querySelectorAll('.botao-tip');
          peopleInput.value ="";
          tipAmountOutput.value ="$ 0,00";
          totalOutput.value ="$ 0,00";
+         inputCustom.value = "";
+         inputCustom.parentElement.dataset.porcentagem = "";
+         inputCustom.placeholder = "Custom";
     }
 
- 
     document.getElementById("reset").addEventListener("click", resetPlaceholders);
  
 
